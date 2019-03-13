7cad436f8c4da33a1fcf99e108ec4c0e5d48a7df
Goran Malenic <023gmk@gmail.com>
2019-03-13 02:51:13 +0100

    datum picker sa potrebnim fajlovima za rezervaciju

---
+++ js/rezervacija.js
@@ -0,0 +1,11 @@
+$('#datum').datepicker({
+	format: 'dd.mm.yyyy.',
+	weekStart: 1,
+	startDate: 'tomorow',
+	language: 'sr-latin',
+	daysOfWeekHighlighted: '0',
+	autoclose: true,
+	orientation: 'bottom left',
+	forceParse: false,
+	todayHighlight: true
+});
